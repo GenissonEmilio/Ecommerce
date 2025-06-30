1df7b58c29ca585d9b65e78ca447ca47158c312c
Genisson Emilio <esgenisson@gmail.com>
2025-06-30 16:05:18 -0300

    Adjust padding in SearchBar input field
    Changed the horizontal padding of the search input from px-4 to px-3 for better alignment or visual consistency.

--- e-commerce/src/components/layout/SearchBar.tsx
+++ e-commerce/src/components/layout/SearchBar.tsx
@@ -11,7 +11,7 @@ export default function SearchBar() {
       <input
         type="text"
         placeholder="Pesquisar Emilian Shop"
-        className="flex-1 px-4 py-2 outline-none text-black"
+        className="flex-1 px-3 py-2 outline-none text-black"
       />
       <button
         type="submit"
